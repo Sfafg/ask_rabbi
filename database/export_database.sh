0d3aaf384d4eb9459eb0cafaace19b06b8395f42
Sławek <slawekzdunek2.3.6@gmail.com>
2025-11-11 17:27:19 +0100

    db full export/import

--- database/export_database.sh
+++ database/export_database.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-docker exec -i ask_rabbi_db pg_dump -U admin -d ask_rabbi -s > "schema.sql"
-docker exec -i ask_rabbi_db pg_dump -U admin -d ask_rabbi -a > "data.sql"
-
-echo "Database export completed!"
+docker exec -i ask_rabbi_db pg_dump -U admin -d ask_rabbi > "full_dump.sql"
---
+++ database/full_dump.sql
@@ -0,0 +1,281 @@
+--
+-- PostgreSQL database dump
+--
+
+\restrict NhcqxElK2sMfzrXCosB9RBVO6eeMEWBAkPKukXRIyXYkCUOFtSgHfDozz57QdDb
+
+-- Dumped from database version 18.0 (Debian 18.0-1.pgdg13+3)
+-- Dumped by pg_dump version 18.0 (Debian 18.0-1.pgdg13+3)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: answers; Type: TABLE; Schema: public; Owner: admin
+--
+
+CREATE TABLE public.answers (
+    id integer NOT NULL,
+    user_id integer NOT NULL,
+    answer_id integer,
+    question_id integer,
+    body text NOT NULL,
+    CONSTRAINT reference CHECK ((((answer_id IS NOT NULL) AND (question_id IS NULL)) OR ((answer_id IS NULL) AND (question_id IS NOT NULL))))
+);
+
+
+ALTER TABLE public.answers OWNER TO admin;
+
+--
+-- Name: answers_id_seq; Type: SEQUENCE; Schema: public; Owner: admin
+--
+
+CREATE SEQUENCE public.answers_id_seq
+    START WITH 0
+    INCREMENT BY 1
+    MINVALUE 0
+    MAXVALUE 2147483647
+    CACHE 1;
+
+
+ALTER SEQUENCE public.answers_id_seq OWNER TO admin;
+
+--
+-- Name: answers_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: admin
+--
+
+ALTER SEQUENCE public.answers_id_seq OWNED BY public.answers.id;
+
+
+--
+-- Name: questions; Type: TABLE; Schema: public; Owner: admin
+--
+
+CREATE TABLE public.questions (
+    id integer NOT NULL,
+    user_id integer NOT NULL,
+    body character varying(1024) NOT NULL
+);
+
+
+ALTER TABLE public.questions OWNER TO admin;
+
+--
+-- Name: questions_id_seq; Type: SEQUENCE; Schema: public; Owner: admin
+--
+
+CREATE SEQUENCE public.questions_id_seq
+    START WITH 0
+    INCREMENT BY 1
+    MINVALUE 0
+    MAXVALUE 2147483647
+    CACHE 1;
+
+
+ALTER SEQUENCE public.questions_id_seq OWNER TO admin;
+
+--
+-- Name: questions_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: admin
+--
+
+ALTER SEQUENCE public.questions_id_seq OWNED BY public.questions.id;
+
+
+--
+-- Name: users; Type: TABLE; Schema: public; Owner: admin
+--
+
+CREATE TABLE public.users (
+    id integer NOT NULL,
+    username character varying(50) NOT NULL,
+    email character varying(100) NOT NULL,
+    type "char" DEFAULT 'u'::"char" NOT NULL
+);
+
+
+ALTER TABLE public.users OWNER TO admin;
+
+--
+-- Name: users_id_seq; Type: SEQUENCE; Schema: public; Owner: admin
+--
+
+CREATE SEQUENCE public.users_id_seq
+    AS integer
+    START WITH 0
+    INCREMENT BY 1
+    MINVALUE 0
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.users_id_seq OWNER TO admin;
+
+--
+-- Name: users_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: admin
+--
+
+ALTER SEQUENCE public.users_id_seq OWNED BY public.users.id;
+
+
+--
+-- Name: answers id; Type: DEFAULT; Schema: public; Owner: admin
+--
+
+ALTER TABLE ONLY public.answers ALTER COLUMN id SET DEFAULT nextval('public.answers_id_seq'::regclass);
+
+
+--
+-- Name: questions id; Type: DEFAULT; Schema: public; Owner: admin
+--
+
+ALTER TABLE ONLY public.questions ALTER COLUMN id SET DEFAULT nextval('public.questions_id_seq'::regclass);
+
+
+--
+-- Name: users id; Type: DEFAULT; Schema: public; Owner: admin
+--
+
+ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_id_seq'::regclass);
+
+
+--
+-- Data for Name: answers; Type: TABLE DATA; Schema: public; Owner: admin
+--
+
+COPY public.answers (id, user_id, answer_id, question_id, body) FROM stdin;
+0	0	\N	0	World!
+\.
+
+
+--
+-- Data for Name: questions; Type: TABLE DATA; Schema: public; Owner: admin
+--
+
+COPY public.questions (id, user_id, body) FROM stdin;
+0	1	Hello?
+\.
+
+
+--
+-- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: admin
+--
+
+COPY public.users (id, username, email, type) FROM stdin;
+0	Hallah	hallah@example.com	r
+1	alice	alice@example.com	u
+2	bob	bob@example.com	u
+\.
+
+
+--
+-- Name: answers_id_seq; Type: SEQUENCE SET; Schema: public; Owner: admin
+--
+
+SELECT pg_catalog.setval('public.answers_id_seq', 0, false);
+
+
+--
+-- Name: questions_id_seq; Type: SEQUENCE SET; Schema: public; Owner: admin
+--
+
+SELECT pg_catalog.setval('public.questions_id_seq', 0, false);
+
+
+--
+-- Name: users_id_seq; Type: SEQUENCE SET; Schema: public; Owner: admin
+--
+
+SELECT pg_catalog.setval('public.users_id_seq', 0, false);
+
+
+--
+-- Name: answers answersid; Type: CONSTRAINT; Schema: public; Owner: admin
+--
+
+ALTER TABLE ONLY public.answers
+    ADD CONSTRAINT answersid PRIMARY KEY (id);
+
+
+--
+-- Name: questions id; Type: CONSTRAINT; Schema: public; Owner: admin
+--
+
+ALTER TABLE ONLY public.questions
+    ADD CONSTRAINT id PRIMARY KEY (id);
+
+
+--
+-- Name: answers one answer; Type: CONSTRAINT; Schema: public; Owner: admin
+--
+
+ALTER TABLE ONLY public.answers
+    ADD CONSTRAINT "one answer" UNIQUE (answer_id);
+
+
+--
+-- Name: users users_email_key; Type: CONSTRAINT; Schema: public; Owner: admin
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_email_key UNIQUE (email);
+
+
+--
+-- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: admin
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: users users_username_key; Type: CONSTRAINT; Schema: public; Owner: admin
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_username_key UNIQUE (username);
+
+
+--
+-- Name: answers answerid; Type: FK CONSTRAINT; Schema: public; Owner: admin
+--
+
+ALTER TABLE ONLY public.answers
+    ADD CONSTRAINT answerid FOREIGN KEY (answer_id) REFERENCES public.answers(id) NOT VALID;
+
+
+--
+-- Name: answers question; Type: FK CONSTRAINT; Schema: public; Owner: admin
+--
+
+ALTER TABLE ONLY public.answers
+    ADD CONSTRAINT question FOREIGN KEY (question_id) REFERENCES public.questions(id);
+
+
+--
+-- Name: answers user; Type: FK CONSTRAINT; Schema: public; Owner: admin
+--
+
+ALTER TABLE ONLY public.answers
+    ADD CONSTRAINT "user" FOREIGN KEY (user_id) REFERENCES public.users(id) NOT VALID;
+
+
+--
+-- PostgreSQL database dump complete
+--
+
+\unrestrict NhcqxElK2sMfzrXCosB9RBVO6eeMEWBAkPKukXRIyXYkCUOFtSgHfDozz57QdDb
+
